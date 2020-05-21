3642a61ebbb242f6100f2723753a61d5c3b89cf5
J3rryCodes <jerinjohnykutty002@gmail.com>
2020-05-22 01:32:26 +0530

    Added Minimax algorithm

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -4,7 +4,7 @@ import numpy as np
 class TicTacToe:
 	flag = {0:' ',1:'X',2:'O'}
 	count = 0
-	depth = 3
+	depth = 2
 	def __init__(self,size,firestPlay):
 		self.bord = np.zeros((size,size))
 		self.size = size 
@@ -16,16 +16,16 @@ class TicTacToe:
 			self.cpuFlag = 1
 
 	def displayBord(self):
-		#print('\x1bc')
-		#header = [i for i in range(0,10)]
-		#bord = tabulate([[self.flag[i] for i in self.bord[j]] for j in range(0,self.size)])
-		#print(tabulate(bord,header,tablefmt="grid",showindex="always"))
-		print(self.bord)
+		print('\x1bc')
+		header = [i for i in range(-1,10)]
+		bord = [[self.flag[i] for i in self.bord[j]] for j in range(0,self.size)]
+		print(tabulate(bord,header,tablefmt="grid",showindex="always"))
+		#print(bord)
+		#print(self.bord)
 
 	def setFlag(self,x,y,flag):
 		if self.bord[x][y] != 0:
 			return
-		print("Flag",flag)
 		self.bord[x][y] = flag
 
 		#Adding count by one ----
